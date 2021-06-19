75715a6e6624cf26698eda9660fe4a9f3275355c
Lourenco <lo.passos93@gmail.com>
2021-06-18 23:59:38 -0300

    feat: add model for company

---
+++ src/model/company.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose';
+
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+export const CompanySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: 'Criterion name required'
+    },
+    logo: {
+      type: String,
+      required: 'Criterion name required'
+    },
+    _id: {
+      type: Number,
+      required: 'Belongs to required'
+    }
+  },
+  { _id: false }
+);
+CompanySchema.plugin(AutoIncrement);
+
+export const CompanyModel = mongoose.model('Company', CompanySchema);
+
+export interface CompanyInputDTO {
+  name: string;
+  logo: string;
+}
+
+export interface CompanyEditDTO {
+  name?: string;
+  logo?: string;
+}
+
