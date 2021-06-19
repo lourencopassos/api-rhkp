b60d343fee9b32361a21ddcbf07c24b4a458c3e7
Lourenco <lo.passos93@gmail.com>
2021-06-19 00:01:05 -0300

    feat: add validation schema for manager

---
+++ src/business/manager/schema.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+export const schema = Joi.object().keys({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).max(30).required(),
+  company_id: Joi.number().positive().integer(),
+  phone: Joi.string().length(11).pattern(/^\d+$/).required(),
+  photo: Joi.string().uri().min(6)
+});
