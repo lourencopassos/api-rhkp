556a489ae0ecb274e0999dd94ba59e2f8925dbbb
Lourenco <lo.passos93@gmail.com>
2021-06-20 22:19:40 -0300

    refactor: change manager interfaces

--- src/types/manager.ts
+++ src/types/manager.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import { Document } from 'mongoose';
 import { ManagerEditDTO, ManagerInputDTO } from '../model';
 
@@ -5,8 +6,25 @@ export interface IManagerDatabase {
   addManager(manager: ManagerInputDTO): Promise<void>;
   getManagerById(id: string): Promise<Document>;
   getManagersByName(managerName: string): Promise<Document | Document[]>;
+  getManagersByEmail(email: string): Promise<Document>;
   deleteManager(id: string): Promise<void>;
   updateManager(manager: ManagerEditDTO, id: string): Promise<void>;
 }
 
-export interface IManagerBusiness {}
+export interface IManagerBusiness {
+  addManager(manager: ManagerInputDTO): Promise<void>;
+  getManagerById(id: string): Promise<Document>;
+  getManagerByName(managerName: string): Promise<Document | Document[]>;
+  getManagerByEmail(email: string): Promise<Document>;
+  deleteManager(id: string): Promise<void>;
+  updateManager(manager: ManagerEditDTO, id: string): Promise<void>;
+}
+
+export interface IManagerController {
+  addManager(req: Request, res: Response): Promise<void>;
+  getManagerById(req: Request, res: Response): Promise<void>;
+  getManagerByName(req: Request, res: Response): Promise<void>;
+  getManagerByEmail(req: Request, res: Response): Promise<void>;
+  deleteManager(req: Request, res: Response): Promise<void>;
+  updateManager(req: Request, res: Response): Promise<void>;
+}
