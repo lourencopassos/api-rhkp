89458aed3551c9fddfff018802d0591a447fde46
Lourenco <lo.passos93@gmail.com>
2021-07-01 18:43:02 -0300

    feat: update manager router

--- src/routes/manager-routes.ts
+++ src/routes/manager-routes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { ManagerBusiness } from '../business';
 import { ManagerController } from '../controller/manager-controller';
 import { ManagerDatabase } from '../data';
+import { HashManager } from '../utils/password-hash';
 
 export const managerRouter = express.Router();
 
 const database = new ManagerDatabase();
-const business = new ManagerBusiness(database);
+const hashManager = new HashManager();
+const business = new ManagerBusiness(database, hashManager);
 const controller = new ManagerController(business);
 
 managerRouter.post('/', controller.addManager);
