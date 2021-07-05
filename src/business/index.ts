5c592036ceb5ac57d6d3cfe539181251ec629ad3
Lourenco <lo.passos93@gmail.com>
2021-07-04 22:09:51 -0300

    feat: add criterion to business index

--- src/business/index.ts
+++ src/business/index.ts
@@ -1,3 +1,4 @@
 export * from './manager/manager-business'
 export * from './employee/employee-business'
 export * from './company/company-business';
+export * from './criterion/criterion-business';
