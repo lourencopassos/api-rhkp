9d5e5866017aa36687d3de31d87e87242223d140
Lourenco <lo.passos93@gmail.com>
2021-07-13 22:36:33 -0300

    refactor: update quantitative evaluation model

--- src/model/quantitative-evaluation.ts
+++ src/model/quantitative-evaluation.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 export const QuantitativeEvaluationSchema = new Schema({
   manager_ratings: {
@@ -8,23 +8,35 @@ export const QuantitativeEvaluationSchema = new Schema({
     required: 'Ratings required'
   },
   evaluator_id: {
-    type: ObjectId,
+    type: Number,
     required: 'Evaluator Id required'
   },
   evaluee_id: {
-    type: ObjectId,
+    type: Number,
     required: 'Evaluee Id required'
   },
-  date: {
+  created_at: {
     type: Date,
     default: Date.now,
-    required: 'Date required'
+    required: 'Created At required'
+  },
+  updated_at: {
+    type: Date
   },
   self_ratings: {
     type: [{ criterion: String, rating: Number, id: Number }],
     required: 'Ratings required'
+  },
+  _id: {
+    type: Number
+  },
+  belongs_to: {
+    type: Number,
+    required: 'Belongs to required'
   }
 });
+
+QuantitativeEvaluationSchema.plugin(AutoIncrement);
 
 export const QuantitativeEvaluationModel = mongoose.model(
   'QuantitativeEvaluation',
@@ -36,9 +48,20 @@ export interface QuantitativeEvaluationInputDTO {
   evaluee_id: string;
   self_ratings: Array<Rating>;
   manager_ratings: Array<Rating>;
+  belongs_to: number;
 }
 
 export interface Rating {
   criterion: string;
-  rating: number
+  rating: number;
+  id: number;
+}
+
+export interface QuantitativeEvaluationEditDTO {
+  evaluator_id?: string;
+  evaluee_id?: string;
+  self_ratings?: Array<Rating>;
+  manager_ratings?: Array<Rating>;
+  belongs_to?: number;
+  updated_at: string;
 }
