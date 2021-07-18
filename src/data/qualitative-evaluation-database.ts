43730a4630f97a806d6fceae33ddc5561925e776
Lourenco <lo.passos93@gmail.com>
2021-07-18 01:16:31 -0300

    feat: add qualitative evaluation database

---
+++ src/data/qualitative-evaluation-database.ts
@@ -0,0 +1,86 @@
+import {
+  QualitativeEvaluationEditDTO,
+  QualitativeEvaluationInputDTO, QualitativeEvaluationModel
+} from '../model';
+import { BaseDatabase } from '.';
+import { Document } from 'mongoose';
+import { IQualitativeEvaluationDatabase } from '../types/qualitative-evaluation';
+
+export class QualitativeEvaluationDatabase
+  extends BaseDatabase
+  implements IQualitativeEvaluationDatabase
+{
+  public addEvaluation = async (
+    evaluation: QualitativeEvaluationInputDTO
+  ): Promise<void> => {
+    try {
+      await this.getConnection();
+      new QualitativeEvaluationModel(evaluation).save();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public getEvaluationById = async (id: number): Promise<Document[]> => {
+    try {
+      await this.getConnection();
+      return await QualitativeEvaluationModel.findById(id).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public getEvaluationsFromEmployeeById = async (
+    evaluee_id: number
+  ): Promise<Document[]> => {
+    try {
+      await this.getConnection();
+      return await QualitativeEvaluationModel.find({ evaluee_id }).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public getEvaluationsFromManagerById = async (
+    evaluator_id: number
+  ): Promise<Document[]> => {
+    try {
+      await this.getConnection();
+      return await QualitativeEvaluationModel.find({ evaluator_id }).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public getCompanyEvaluations = async (
+    belongs_to: number
+  ): Promise<Document[]> => {
+    try {
+      await this.getConnection();
+      return await QualitativeEvaluationModel.find({ belongs_to }).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public deleteEvaluation = async (id: number) => {
+    try {
+      await this.getConnection();
+      await QualitativeEvaluationModel.findByIdAndRemove(id).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  public updateEvaluation = async (
+    evaluation: QualitativeEvaluationEditDTO,
+    id: number
+  ) => {
+    try {
+      await this.getConnection();
+      await QualitativeEvaluationModel.findByIdAndUpdate(id, evaluation).exec();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+}
