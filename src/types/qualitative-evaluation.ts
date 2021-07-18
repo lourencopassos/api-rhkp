55b3fb1530f7e55bfcf9dc4d9bafd51d2fe6cb1e
Lourenco <lo.passos93@gmail.com>
2021-07-18 01:14:24 -0300

    feat: add qualitative types

---
+++ src/types/qualitative-evaluation.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { Document } from 'mongoose';
+import {
+  QualitativeEvaluationEditDTO,
+  QualitativeEvaluationInputDTO
+} from '../model';
+
+export interface IQualitativeEvaluationDatabase {
+  addEvaluation(evaluation: QualitativeEvaluationInputDTO): Promise<void>;
+  getEvaluationById(id: number): Promise<Document[]>;
+  getEvaluationsFromEmployeeById(id: number): Promise<Document[]>;
+  getEvaluationsFromManagerById(id: number): Promise<Document[]>;
+  getCompanyEvaluations(belongs_to: number): Promise<Document[]>;
+  deleteEvaluation(id: number): Promise<void>;
+  updateEvaluation(
+    evaluation: QualitativeEvaluationEditDTO,
+    id: number
+  ): Promise<void>;
+}
+
+export interface IQualitativeEvaluationBusiness {
+  addEvaluation(evaluation: QualitativeEvaluationInputDTO): Promise<void>;
+  getEvaluationById(id: number): Promise<Document[]>;
+  getEvaluationsFromEmployeeById(id: number): Promise<Document[]>;
+  getEvaluationsFromManagerById(id: number): Promise<Document[]>;
+  getCompanyEvaluations(belongs_to: number): Promise<Document[]>;
+  deleteEvaluation(id: number): Promise<void>;
+  updateEvaluation(
+    evaluation: QualitativeEvaluationEditDTO,
+    id: number
+  ): Promise<void>;
+}
+
+export interface IQualitativeEvaluationController {
+  addEvaluation(req: Request, res: Response): Promise<void>;
+  getEvaluationById(req: Request, res: Response): Promise<void>;
+  getEvaluationsFromEmployeeById(req: Request, res: Response): Promise<void>;
+  getEvaluationsFromManagerById(req: Request, res: Response): Promise<void>;
+  getCompanyEvaluations(req: Request, res: Response): Promise<void>;
+  deleteEvaluation(req: Request, res: Response): Promise<void>;
+  updateEvaluation(req: Request, res: Response): Promise<void>;
+}
