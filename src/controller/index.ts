6b3aed71641a97d9dff3412a971c80ecd8316570
Lourenco <lo.passos93@gmail.com>
2021-07-18 17:07:04 -0300

    feat: add qualitative evaluation routes to index

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -2,5 +2,6 @@ export * from './company-controller';
 export * from './employee-controller';
 export * from './manager-controller';
 export * from './quantitative-evaluation-controller';
-export * from './criterion-controller'
+export * from './criterion-controller';
+export * from './qualitative-evaluation-controller';
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,5 @@ export * from './manager-routes';
 export * from './employee-routes';
 export * from './company-routes';
 export * from './quantitative-evaluation-routes';
-export * from './criterions-routes'
+export * from './criterions-routes';
+export * from './qualitative-evaluation-routes';
