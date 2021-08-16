3eff7c9f871ea81ec02f56eaef197524fb1efe38
Lourenco <lo.passos93@gmail.com>
2021-08-15 22:31:41 -0300

    feat: change server port

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(
   qualitativeEvaluationRouter
 );
 
-export const server = app.listen(3000, () => {
+export const server = app.listen(4000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
