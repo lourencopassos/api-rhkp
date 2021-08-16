baa00fae91e64493afc7654f7c9522699694c5b9
Lourenco <lo.passos93@gmail.com>
2021-08-15 22:36:26 -0300

    refactor: auto increment id

--- src/model/quantitative-evaluation.ts
+++ src/model/quantitative-evaluation.ts
@@ -2,45 +2,52 @@ import mongoose, { Schema } from 'mongoose';
 
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-export const QuantitativeEvaluationSchema = new Schema({
-  manager_ratings: {
-    type: [{ criterion: String, rating: Number, id: Number }],
-    required: 'Ratings required'
+export const QuantitativeEvaluationSchema = new Schema(
+  {
+    manager_ratings: {
+      type: [{ criterion: String, rating: Number, id: Number }],
+      required: 'Ratings required'
+    },
+    evaluator_id: {
+      type: Number,
+      required: 'Evaluator Id required'
+    },
+    evaluee_id: {
+      type: Number,
+      required: 'Evaluee Id required'
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+      required: 'Created At required'
+    },
+    updated_at: {
+      type: Date
+    },
+    self_ratings: {
+      type: [{ criterion: String, rating: Number, id: Number }],
+      required: 'Ratings required'
+    },
+    id: {
+      type: Number
+    },
+    company_id: {
+      type: Number,
+      required: 'Belongs to required'
+    }
   },
-  evaluator_id: {
-    type: Number,
-    required: 'Evaluator Id required'
-  },
-  evaluee_id: {
-    type: Number,
-    required: 'Evaluee Id required'
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-    required: 'Created At required'
-  },
-  updated_at: {
-    type: Date
-  },
-  self_ratings: {
-    type: [{ criterion: String, rating: Number, id: Number }],
-    required: 'Ratings required'
-  },
-  _id: {
-    type: Number
-  },
-  company_id: {
-    type: Number,
-    required: 'Belongs to required'
-  }
-});
+  { _id: false }
+);
 
-QuantitativeEvaluationSchema.plugin(AutoIncrement);
+QuantitativeEvaluationSchema.plugin(AutoIncrement, {
+  id: 'quantitative_id',
+  inc_field: '_id'
+});
 
 export const QuantitativeEvaluationModel = mongoose.model(
   'QuantitativeEvaluation',
-  QuantitativeEvaluationSchema
+  QuantitativeEvaluationSchema,
+  'quantitative evaluation'
 );
 
 export interface QuantitativeEvaluationInputDTO {
